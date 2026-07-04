7f6c2d09be61eaeda654576146c34fe1c4357d27
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-07-04 13:17:48 +0545

    feature: add expense creation with CSV file storage

--- Expense_Tracker/src/ExpenseManager.java
+++ Expense_Tracker/src/ExpenseManager.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
-public class ExpenseManager {
+public class ExpenseManager  {
     Scanner sc = new Scanner(System.in);
 
-    void addExpense() {
+    void addExpense(){
         System.out.println("========== ADD EXPENSE ==========");
         System.out.print("Enter Category: ");
         String category = sc.nextLine();
@@ -18,9 +18,13 @@ public class ExpenseManager {
         int expenseId = 1;
         Expense exp = new Expense();
         exp.createExpense(expenseId, category, reason, amount, date);
-        exp.displayExpense();
         FileManager fm = new FileManager();
-        fm.saveExpense(exp);
-    }
+        if(fm.saveExpense(exp)){
+            exp.displayExpense();
+            System.out.println("Expense Added Successfully...");
+        } else {
+            System.out.println("Expense not Saved! please try Again...");
+        }
+     }
 
 }
--- Expense_Tracker/src/FileManager.java
+++ Expense_Tracker/src/FileManager.java
@@ -1,8 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileManager {
 
-    boolean saveExpense(Expense expense) {
+    boolean saveExpense(Expense exp) {
+        try{
+            BufferedWriter bw = new BufferedWriter(new FileWriter("../database/expenses.csv",true));
         System.out.println("Saving Expense...");
+
+        bw.write(exp.expenseId + ", " + exp.category + ", " + exp.reason + ", " + exp.amount + ", " + exp.date);
+        bw.newLine();
+        bw.close();
         return true;
+
+        } catch(IOException e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
+    
 }
